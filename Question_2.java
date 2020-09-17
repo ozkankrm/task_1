0973b317cc57ea7c8f5cecb4b1f7bc692b9c2c64
ozkankrm <71409837+ozkankrm@users.noreply.github.com>
2020-09-17 21:12:58 +0100

    Add files via upload

---
+++ Question_2.java
@@ -0,0 +1,12 @@
+package day_2;
+
+public class Question_2 {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Turing, Alan");
+		
+	}
+	
+
+}
---
+++ Question_8.java
@@ -0,0 +1,22 @@
+package day_2;
+
+public class Question_8 {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		
+		System.out.println("******          *************       **********          ");
+		System.out.println("**    **               **           **                  ");
+		System.out.println("**     **              **           **                  ");
+		System.out.println("**      **             **           **                  ");
+		System.out.println("**      **             **           ********            ");
+		System.out.println("**      **      **     **           **                  ");
+		System.out.println("**     **        **    **           **                   ");
+		System.out.println("**    **          **  **            **                   ");
+		System.out.println("*****              ****             **********           ");
+		
+		
+	}
+
+}
